7e2db8f515b86fff9d66ed3ffda536d3ee1aadca
NASSER1221 <alnassri1221@gmail.com>
2025-07-06 16:35:22 +0400

    practice2

---
+++ practice2.java
@@ -0,0 +1,18 @@
+package basics;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class practice2 {
+    public static void main(String[] args){
+
+        List<String> list = Arrays.asList("nasser", "abdullah");
+
+        for(String st: list){
+            StringBuilder string1= new StringBuilder(new StringBuilder(st).reverse().toString());
+            System.out.println(string1);
+
+        }
+
+    }
+}
